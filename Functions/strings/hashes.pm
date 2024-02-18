888051e6aa9b078789cc273fea491385f2ce5a8d
Jake-perl <159278555+Jake-perl@users.noreply.github.com>
2024-02-18 22:25:44 +0000

    Finish hashes.pm

--- Functions/strings/hashes.pm
+++ Functions/strings/hashes.pm
@@ -1,1 +1,16 @@
 # A hash/associative array is an unordered set of scalars.
+# for example, you can make one for abbreviated day names.
+my %longday = ("Mon", "Monday", "Tue", "Tuesday", "Wed", "Wednesday", "Thu", "Thursday", "Fri", "Friday", "Sat", "Saturday", "Sun", "Sunday");
+# thats too difficult to read. we can make it easier like this
+my %longday = (
+"Mon" => "Monday",
+"Tue" => "Tuesday",
+"Wed" => "Wednesday",
+"Thu" => "Thursday",
+"Fri" => "Friday",
+"Sat" => "Saturday",
+"Sun" => "Sunday",
+
+# another thing you can do with hashes
+my %wife;
+%wife{"My father"} = "My mother"
